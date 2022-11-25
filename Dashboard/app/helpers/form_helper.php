aa21bdb1c7b0135f208299ccd63da34fc8a4a7eb
TF <50201452+taufik-fauzi@users.noreply.github.com>
2022-11-25 18:51:50 +0700

    Add files via upload

---
+++ Dashboard/app/helpers/form_helper.php
@@ -0,0 +1,33 @@
+<?php
+
+// untuk membandingkan dua jenis, lalu bisa di selected
+function getSelected($s1, $s2)
+{
+    if($s1 == $s2)
+    {
+        echo "selected";
+    }
+}
+
+// untuk membandingkan dua jenis, lalu bisa di checked
+function getChecked($s1, $s2)
+{
+    if($s1 == $s2)
+    {
+        echo "checked";
+    }
+}
+
+// untuk checked genre
+function getGenre($cari, $string)
+{
+    $array = explode(",", $string);
+    $caridata = in_array($cari, $array);
+    if ($caridata)
+    {   
+        echo "checked";
+    }
+}
+
+// explode = untuk merubah string menjadi array
+// in_array = mencari data yang sesuai didalam array
