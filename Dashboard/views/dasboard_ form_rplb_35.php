b0cbf10da06a6409f6bf4a973dedc5fd924a9c5f
TF <50201452+taufik-fauzi@users.noreply.github.com>
2022-11-26 15:12:09 +0700

    Update dasboard_ form_rplb_35.php

--- Dashboard/views/dasboard_ form_rplb_35.php
+++ Dashboard/views/dasboard_ form_rplb_35.php
@@ -2,6 +2,8 @@
 
 session_start();
 
+require_once("../controller/c_dashboard.php");
+
 if (!isset($_SESSION["login"])) {
     header("Location: ../models/login.php");
     exit;
@@ -26,7 +28,8 @@ if (!isset($_SESSION["login"])) {
 
 <body>
 
-    <!-- sidebar -->
+    <!--Start Sidebar  -->
+
     <div class="sidebar close">
 
         <!-- logo -->
@@ -41,14 +44,11 @@ if (!isset($_SESSION["login"])) {
 
                 <!-- menu -->
 
-                <a href="dashboard_rplb_35.html">
+                <a href="dashboard_rplb_35.php">
                     <i class='bx bx-grid-alt'></i>
                     <span class="link_name">Dashboard</span>
                 </a>
 
-                <ul class="sub-menu blank">
-                    <li><a class="link_name" href="#">Category</a></li>
-                </ul>
             </li>
             <li>
 
@@ -57,45 +57,41 @@ if (!isset($_SESSION["login"])) {
                 <div class="iocn-link">
                     <a href="#">
                         <i class='bx bx-collection'></i>
-                        <span class="link_name">Category</span>
+                        <span class="link_name">List Data</span>
                     </a>
-                    <i class='bx bxs-chevron-down arrow'></i>
+                    <!-- <i class='bx bxs-chevron-down arrow'></i> -->
                 </div>
                 <ul class="sub-menu">
-                    <li><a class="link_name" href="#">Menu</a></li>
-                    <li><a href="dasboard_listdata_rplb_35.php">List Tabel</a></li>
-                    <li><a href="dasboard_detaildata_rplb_35.php">Detail Data</a></li>
-                    <li><a href="#">Menu</a></li>
+                    <li><a href="dasboard_listdata_rplb_35.php">List Data</a></li>
                 </ul>
             </li>
 
             <!-- dropdown -->
             <li>
                 <div class="iocn-link">
-                    <a href="#">
+                    <a href="dasboard_detaildata_rplb_35.php">
                         <i class='bx bx-book-alt'></i>
-                        <span class="link_name">Posts</span>
+                        <span class="link_name">Detail Data</span>
                     </a>
-                    <i class='bx bxs-chevron-down arrow'></i>
+                    <!-- <i class='bx bxs-chevron-down arrow'></i> -->
                 </div>
                 <ul class="sub-menu">
-                    <li><a class="link_name" href="#">Posts</a></li>
-                    <li><a href="#">Menu</a></li>
-                    <li><a href="#">Menu</a></li>
-                    <li><a href="#">Menu</a></li>
+                    <li><a href="dasboard_detaildata_rplb_35.php">Detail Data</a></li>
                 </ul>
             </li>
             <li>
 
-                <!-- menu -->
-
-                <a href="#">
-                    <i class='bx bx-pie-chart-alt-2'></i>
-                    <span class="link_name">Analytics</span>
-                </a>
-                <ul class="sub-menu blank">
-                    <li><a class="link_name" href="#">Analytics</a></li>
+                <div class="iocn-link">
+                    <a href="dasboard_form_rplb_35.php">
+                        <i class='bx bx-pie-chart-alt-2'></i>
+                        <span class="link_name">Form</span>
+                    </a>
+                    <i class='bx bxs-chevron-down arrow'></i>
+                </div>
+                <ul class="sub-menu">
+                    <li><a href="dasboard_form_rplb_35.php">Form</a></li>
                 </ul>
+
             </li>
             <li>
 
@@ -134,13 +130,16 @@ if (!isset($_SESSION["login"])) {
             <li>
                 <!-- menu -->
 
-                <a href="#">
+                <a href="../models/logout.php">
                     <i class='bx bx-log-out'></i>
                     <span class="link_name">Log out</span>
                 </a>
             </li>
         </ul>
     </div>
+
+    <!-- End Sidebar -->
+
 
     <!-- home -->
 
@@ -169,64 +168,71 @@ if (!isset($_SESSION["login"])) {
                 </div>
 
                 <div class="container-form">
-                    <form action="" method="POST" autocomplete="off">
+                    <form action="" method="POST" autocomplete="off" enctype="multipart/form-data">
                         <div class="box">
                             <div>
+                                <!-- KODE BARANG -->
                                 <label for="kbuku">Kode Barang : </label><br>
-                                <input type="text" name="kbuku" id="kbuku" required><br>
+                                <input type="text" name="txtkode" id="kbuku" required><br>
+
+                                <!-- JUDUL BARANG -->
                                 <label for="jbuku">Judul Barang : </label><br>
-                                <input type="text" name="jbuku" id="jbuku" required><br>
+                                <input type="text" name="txtjudul" id="jbuku" required><br>
+
+                                <!-- PENERBIT BARANG -->
                                 <label for="perbuku">Penerbit Barang : </label><br>
-                                <select id="perbuku" name="perbuku">
+                                <input type="text" name="txtpenerbit" id="pbrang" required><br>
+
+                                <!-- STOK BARANG -->
+                                <label for="perbuku">Stok Barang : </label><br>
+                                <input type="number" name="intstok" id="sbrang" required><br>
+
+                                <!-- <select id="perbuku" name="perbuku">
                                     <option disabled selected hidden value="Penerbit">Penerbit</option>
                                     <option value="Data 1">Data </option>
                                     <option value="Data 1">Data </option>
                                     <option value="Data 1">Data </option>
                                     <option value="Data 1">Data </option>
                                     <option value="Data 1">Data </option>
-                                </select>
+                                </select> -->
+
                             </div>
 
                             <div class="space">
+
+
+                                <!-- JENIS BARANG -->
                                 <label for="jbarang">Jenis Barang :</label><br>
-                                <input type="radio" name="data" id="data" value="Data">
-                                <label for="Data">Data 1</label><br>
-                                <input type="radio" name="data" id="data" value="Data">
-                                <label for="Data">Data 2</label><br>
-                                <input type="radio" name="data" id="data" value="Data">
-                                <label for="Data">Data 3</label><br><br>
+                                <input type="radio" name="rdjenis" id="data" value="Pakaian">Pakaian<br>
+                                <input type="radio" name="rdjenis" id="data" value="Makeup">Makeup<br>
+                                <input type="radio" name="rdjenis" id="data" value="Alat">Alat<br>
+                                <input type="radio" name="rdjenis" id="data" value="Rumah Tangga">Rumah Tangga<br>
+                                <input type="radio" name="rdjenis" id="data" value="Mainan">Mainan<br>
+                                <input type="radio" name="rdjenis" id="data" value="Desain">Desain<br><br>
 
-                                <label for="chbarang">Checkbox Barang :</label><br>
-                                <input type="checkbox" name="" value="Data">
-                                <label for="Data">Data 1</label><br>
-                                <input type="checkbox" name="" value="Data">
-                                <label for="Data">Data 1</label><br>
-                                <input type="checkbox" name="" value="Data">
-                                <label for="Data">Data 1</label><br>
-                                <input type="checkbox" name="" value="Data">
-                                <label for="Data">Data 1</label><br>
+                                <!-- SENSITIVITAS BARANG -->
+                                <label for="chbarang">Sensitivitas Barang :</label><br>
+                                <input type="checkbox" name="cbarang" value="Mudah Pecah">Mudah Pecah<br>
+                                <input type="checkbox" name="cbarang" value="Anti Pecah">Anti Pecah<br>
                             </div>
 
                             <div class="space">
+
+                                <!-- GAMBAR BARANG -->
                                 <label for="gbrang">Gambar Barang : </label><br>
-                                <input type="file" name="file_barang">
+                                <input type="file" name="file_barang" required>
                                 <br><br><br><br>
+
+                                <!-- CATATAN BARANG -->
                                 <label for="ctatan">Catatan Barang : </label><br>
                                 <textarea id="catbarang" name="catbarang" rows="3" cols="0"></textarea>
+                                <br><br>
+
+                                <!-- SUBMIT -->
+                                <input type="submit" name="submit" required>
                             </div>
                     </form>
                 </div>
-
-
-                <!-- <div class="text">
-                            <label for="kelamin">Jenis Kelamin</label>
-                            <input type="radio" name="gender" id="laki" value="Laki-Laki">
-                            <label for="male">Laki-Laki</label> -->
-
-
-
-
-
             </div>
         </div>
         </div>
